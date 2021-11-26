8ef76261ad83fd1791d20f6459e5b25b81c7872e
Hugo-Ap <araparhug7@gmail.com>
2021-11-26 03:28:49 -0500

    hotfix

--- feedback-service/src/main/java/com/dystopia/feedbackservice/controller/QualificationController.java
+++ feedback-service/src/main/java/com/dystopia/feedbackservice/controller/QualificationController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping()
 public class QualificationController {
     @Autowired
     private QualificationService qualificationService;

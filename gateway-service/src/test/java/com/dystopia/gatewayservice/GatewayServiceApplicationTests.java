8ae3072ddaa1ef87f55a3bad305ff5af115b145c
Hugo-Ap <araparhug7@gmail.com>
2021-11-26 00:25:49 -0500

    Update of the gateway-service

--- gateway-service/src/test/java/com/dystopia/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/dystopia/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.dystopia.gateway;
+package com.dystopia.gatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayApplicationTests {
+class GatewayServiceApplicationTests {
 
     @Test
     void contextLoads() {
